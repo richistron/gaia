fa640c7c0a0fe44f0455e410bfc50804bbbabe72
Ricardo Rivas <richistron@gmail.com>
2019-04-03 23:32:31 -0600

    adds navigation

--- src/components/organisms/Navigation.js
+++ src/components/organisms/Navigation.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {Link} from 'react-router-dom';
+import {withRouter} from 'react-router';
+import propTypes from 'prop-types';
 
-const Navigation = () => {
+const Navigation = props => {
+  // initialize routes in ios and android
+  useEffect(() => {
+    if (!props.location.pathname) {
+      props.history.push('/');
+    }
+  }, []);
   return (
-    <div>nav</div>
+    <nav className={'navbar sticky-top navbar-light bg-light'}>
+      <Link className={'navbar-brand'} to={'/'}> Brand </Link>
+      <button
+        className={ 'navbar-toggler' }
+        type={ 'button'  }
+        data-toggle={ 'collapse' }
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+    </nav>
   );
 };
 
-export default Navigation;
+Navigation.propTypes = {
+  location: propTypes.shape({
+    pathname: propTypes.string
+  }).isRequired,
+  history: propTypes.shape({
+    push: propTypes.func,
+  }).isRequired,
+};
+
+export default withRouter(Navigation);
